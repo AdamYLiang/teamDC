db63f2abdc415d1c5f4d72af4708e0a3cb7e0e37
Andrew Cualing <ayc300@nyu.edu>
2016-09-27 15:08:53 -0400

    created gate/switch mechanic with OnTriggerEnter

--- TeamDC/Assets/Scripts/mazeMechanics/basicSwitch.cs
+++ TeamDC/Assets/Scripts/mazeMechanics/basicSwitch.cs
@@ -1,12 +1,21 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
 public class basicSwitch : MonoBehaviour {
 
     [SerializeField]
-    GameObject switchBlock;
+    List<GameObject> blocksToTurnOff = new List<GameObject>(); //create a list of objects to turn off, assign number and objects in inspector
 
+    
 
 	void OnTriggerEnter (Collider activator) {
+        if (activator.tag == "Player") //if player enters "the switch"
+        {
+            foreach (GameObject block in blocksToTurnOff) //set all gameobjects in the list to false
+            {
+                block.SetActive(false);
+            }
+        }
 	}
 }
