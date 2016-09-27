96d5ba32c9d0e81531282ffbde4dd1fb90a104d6
Andrew Cualing <ayc300@nyu.edu>
2016-09-27 14:42:23 -0400

    basic switch gate implementation in progress

---
+++ TeamDC/Assets/Scripts/mazeMechanics/basicSwitch.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class basicSwitch : MonoBehaviour {
+
+    [SerializeField]
+    GameObject switchBlock;
+
+
+	void OnTriggerEnter (Collider activator) {
+	}
+}
