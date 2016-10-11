d9ed0515fa8ab7061f5a1bf87fffa8db5bc6859f
unknown <ayl292@nyu.edu>
2016-10-11 16:29:59 -0400

    working lights and level consistency WOWOWOW

--- TeamDC/Assets/Scripts/unusedFiles/LightFade.cs
+++ TeamDC/Assets/Scripts/unusedFiles/LightFade.cs
@@ -2,42 +2,44 @@
 using System.Collections;
 
 public class LightFade : MonoBehaviour {
-    float highIntensity = 1.0f;
-    float lowIntensity = 0.0f;
-    public float fadeSpeed = 0.1f;
-    public float fadeInCap = 0.99f;
-    public float fadeOutCap = 0.01f;
-    bool fadeIn;
-    bool fadeOut;
+	public float highIntensity;
+    public float lowIntensity;
+    public float fadeSpeed;
+    public float fadeInCap;
+	public float fadeOutCap;
+	bool fadingOut;
+	bool fadingIn;
 
 
 	// Use this for initialization
 	void Start () {
+
+		fadeInCap = 4.90f;
+		fadeOutCap = 0.10f;
+		highIntensity = 5.0f;
+		lowIntensity = 0.0f;
+		fadeSpeed = 3;
+		fadingOut = false;
+		fadingIn = false;
 	
 	}
 	
 	// Update is called once per frame
 	void Update () {
-        if(gameObject.GetComponent<Light>().intensity > fadeInCap)
-        { fadeOut = true;
-            fadeIn = false;
-        }
-        if (gameObject.GetComponent<Light>().intensity < fadeOutCap)
-        {
-            fadeOut = false;
-            fadeIn = true;
-        }
 
-        if (fadeIn == true)
+		if ((gameObject.GetComponent<Light>().intensity > fadeInCap) || fadingOut)
         {
-            if (gameObject.GetComponent<Light>().intensity < highIntensity)
-                gameObject.GetComponent<Light>().intensity = Mathf.Lerp(gameObject.GetComponent<Light>().intensity, highIntensity, fadeSpeed * Time.deltaTime);
-        }
-       if (fadeOut == true)
-        {
-            if (gameObject.GetComponent<Light>().intensity < highIntensity)
-                gameObject.GetComponent<Light>().intensity = Mathf.Lerp(gameObject.GetComponent<Light>().intensity, lowIntensity, fadeSpeed * Time.deltaTime);
-        }
+			Debug.Log("fade out please " + gameObject.GetComponent<Light>().intensity);
+			fadingOut = true;
+			fadingIn = false;
+            gameObject.GetComponent<Light>().intensity = Mathf.Lerp(gameObject.GetComponent<Light>().intensity, lowIntensity, fadeSpeed * Time.deltaTime);
+		}
 
-    }
-}
+		if ((gameObject.GetComponent<Light>().intensity < fadeOutCap) || fadingIn)
+        {
+			fadingIn = true;
+			fadingOut = false;
+			Debug.Log("fade in please " + gameObject.GetComponent<Light>().intensity);
+            gameObject.GetComponent<Light>().intensity = Mathf.Lerp(gameObject.GetComponent<Light>().intensity, highIntensity, fadeSpeed * Time.deltaTime);
+		}
+	}}
