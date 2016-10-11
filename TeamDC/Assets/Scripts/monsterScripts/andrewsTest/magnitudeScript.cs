994e9d4d30bc212ce0ed98926b9b892acf9e4e17
Erik <emm615@nyu.edu>
2016-10-11 14:00:10 -0400

    issues pulling andrew branch reverting to before.

---
+++ TeamDC/Assets/Scripts/monsterScripts/andrewsTest/magnitudeScript.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class magnitudeScript : MonoBehaviour {
+
+    [SerializeField]
+    Transform player1;
+
+    [SerializeField]
+    Transform player2;
+
+    public float mag;
+    public Text text;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        mag = (player2.position - player1.position).magnitude;
+        text.text = mag.ToString();
+
+
+	}
+}
