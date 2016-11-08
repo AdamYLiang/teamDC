ed7843261179fded8ef91b71a598676d8b14a0e7
Andrew Cualing <ayc300@nyu.edu>
2016-11-08 14:56:31 -0500

    playtest version commit! todo- hookup level5 ui

--- TeamDC/Assets/Scripts/monsterScripts/patrollingMonster.cs
+++ TeamDC/Assets/Scripts/monsterScripts/patrollingMonster.cs
@@ -15,6 +15,8 @@ public class patrollingMonster : MonoBehaviour {
     [SerializeField]
     float raycastDistance = 3f;
     public LayerMask layerM;
+    public bool goForward = true;
+    public bool goRight = false;
 
     void Start ()
     {
@@ -23,17 +25,41 @@ public class patrollingMonster : MonoBehaviour {
     }
 
 	void Update () {
-
-        transform.position += Vector3.forward * speed  * Time.deltaTime; //makes enemy move
-
-        Ray ray = new Ray(transform.position, transform.forward);
-        Debug.DrawRay(transform.position, transform.forward * raycastDistance, Color.red);
-        RaycastHit rayHitInfo = new RaycastHit();
-        
-        if (Physics.Raycast(ray, out rayHitInfo, raycastDistance)) //when the raycast hits something
+        if (goForward)
         {
-            transform.Rotate(0f, 180f, 0f);  //turn around and reverse speed (so it goes in the opposite direction
-            speed *= -1;
+            transform.position += Vector3.forward * speed * Time.deltaTime; //makes enemy move
+            Ray ray = new Ray(transform.position, transform.forward);
+            Debug.DrawRay(transform.position, transform.forward * raycastDistance, Color.red);
+            RaycastHit rayHitInfo = new RaycastHit();
+
+            if (Physics.Raycast(ray, out rayHitInfo, raycastDistance)) //when the raycast hits something
+            {
+                transform.Rotate(0f, 180f, 0f);  //turn around and reverse speed (so it goes in the opposite direction
+                speed *= -1;
+            }
+        }
+        if (goRight)
+        {
+            transform.position += Vector3.right * speed * Time.deltaTime;
+            Ray ray = new Ray(transform.position, transform.right);
+            Debug.DrawRay(transform.position, transform.right * raycastDistance, Color.red);
+            RaycastHit rayHitInfo = new RaycastHit();
+
+            if (Physics.Raycast(ray, out rayHitInfo, raycastDistance)) //when the raycast hits something
+            {
+                transform.Rotate(0f, 180f, 0f);  //turn around and reverse speed (so it goes in the opposite direction
+                speed *= -1;
+            }
+        }
+
+        //Ray ray = new Ray(transform.position, transform.forward);
+        //Debug.DrawRay(transform.position, transform.forward * raycastDistance, Color.red);
+        //RaycastHit rayHitInfo = new RaycastHit();
+        
+//if (Physics.Raycast(ray, out rayHitInfo, raycastDistance)) //when the raycast hits something
+      //  {
+//transform.Rotate(0f, 180f, 0f);  //turn around and reverse speed (so it goes in the opposite direction
+       //     speed *= -1;
         }
 	}
-}
+
