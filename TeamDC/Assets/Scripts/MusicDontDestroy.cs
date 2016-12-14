3ba74e9bbfca1259946cfd7edde14f594f712d27
Erik <emm615@nyu.edu>
2016-12-14 15:21:44 -0500

    added sound files and dontdestroyonload bgm

---
+++ TeamDC/Assets/Scripts/MusicDontDestroy.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MusicDontDestroy : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        DontDestroyOnLoad(this.gameObject);
+    }
+}
