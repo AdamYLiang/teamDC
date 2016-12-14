f781ccf7b34eaf0bdbb435aaeebce86f6ef6cbc3
ayl292 <ayl292@nyu.edu>
2016-12-14 17:46:21 -0500

    finished sound and everything, ready to present

---
+++ TeamDC/Assets/DoorSoundCrashScript.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DoorSoundCrashScript : MonoBehaviour {
+
+	public AudioSource doorHit;
+
+	public void playDoorCrash(){
+		doorHit.Play();
+	}
+}
